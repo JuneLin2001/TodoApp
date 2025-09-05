c7c70a42b0f58898cbcfc82619d5a4ee3defe46d
Yen-Chun Lin <asd900606@gmail.com>
2025-09-05 17:22:52 +0800

    feat: initialize project

---
+++ src/components/TodoApp/index.tsx
@@ -0,0 +1,16 @@
+const TodoApp: React.FC = () => {
+  // 請在此處實作狀態管理和相關邏輯
+
+  // 請實作以下功能：
+  // 1. 待辦事項列表狀態管理
+  // 2. 新增待辦事項功能
+  // 3. 切換完成狀態功能
+  // 4. 刪除待辦事項功能
+  // 5. 篩選功能
+  // 6. localStorage 持久化儲存
+  // 7. 編輯待辦事項功能
+
+  return <div className="todo-app">{/* 請在此處實作 UI */}</div>;
+};
+
+export default TodoApp;
